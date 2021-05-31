cab695b855f017dfcc8670037e13423541a1e3d0
Jamie Fristrom <jafristr@microsoft.com>
2021-05-31 15:38:13 -0700

    Nothing to do with main project but doing wikibooks monad state tutorial and recording it for posterity

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,18 +1,206 @@
+{-# LANGUAGE InstanceSigs #-}
+
 module Lib
     ( someFunc
     , crappygcd
---    , pop
---    , push
-    , Stack
---    , stackManip
+    , composition
+    , TurnstileOutput(..)
+    , TurnstileInput(..)
+    , TurnstileState(..)
+    , State(..)
+    , coin
+    , push
+    , coinSX
+    , pushSX
+    , monday
+    , tuesday
+    , regularPerson
+    , distractedPerson
+    , hastyPerson
+    , luckyPair
+    , regularPersonSX
+    , distractedPersonSX
+    , hastyPersonSX
+    , luckyPairSX
+    , turnSX
+    , testTurnstile
+    , get
+    , put
+    , modify
+    , gets
+    -- , tick
+    -- , pop
+    -- , push
+    -- , Stack(..)
+    -- , stackManip
 --    , W
     ) where
 
+import Control.Monad
+
 newtype State s a = State { runState :: s -> (a, s) }
 
+state :: (s -> (a, s)) -> State s a
+state = State
+
+instance Functor (State s) where
+    fmap = liftM
+
+instance Applicative (State s) where
+    pure = return
+    (<*>) = ap
+
+instance Monad (State s) where
+    return :: a -> State s a  -- illegal in standard haskell
+    return x = state ( \ s -> (x, s) )
+
+    (>>=) :: State s a -> (a -> State s b) -> State s b
+    stateXform0 >>= valueToStateXformFunc = stateXformResult where
+        stateXform0' = runState stateXform0
+        valueToStateXform = runState . valueToStateXformFunc  -- composing the value to state func with a state transition
+        stateXformResult' state0 = (yResult, state2) where
+            (xResult, state1) = stateXform0' state0
+            (yResult, state2) = valueToStateXform xResult state1
+        stateXformResult = state stateXformResult'
+
+put :: s -> State s ()
+put newState = state $ \_ -> ((), newState)
+
+get :: State s s
+get = state $ \s -> (s, s) 
+
+modify :: (s -> s) -> State s ()
+modify mutator = state $ \s -> ((), mutator s)
+
+gets :: (s -> a) -> State s a
+gets mutator = state $ \s -> (mutator s, s)
 
 
-type Stack = [Integer]
+composition :: (s -> (a,s)) -> (a -> (s -> (b, s ))) -> s -> (b,s)
+composition f g = \state0 -> let ( fResult, fStateResult ) = f state0 
+                              in g fResult fStateResult
+
+
+data TurnstileState = Locked | Unlocked
+    deriving (Eq, Show)
+
+data TurnstileOutput = Thank | Open | Tut
+    deriving (Eq, Show)
+
+data TurnstileInput = Coin | Push
+    deriving (Eq, Show)
+
+coin :: TurnstileState -> (TurnstileOutput, TurnstileState)
+coin _ = (Thank, Unlocked)
+
+push :: TurnstileState -> (TurnstileOutput, TurnstileState)
+push Unlocked = (Open, Locked)
+push Locked   = (Tut, Locked)
+--import Control.Monad.Trans.State
+
+coinSX :: State TurnstileState TurnstileOutput
+coinSX = do
+    s0 <- get
+    put Unlocked
+    return Thank
+
+pushSX :: State TurnstileState TurnstileOutput
+pushSX = state push
+
+turnSX :: TurnstileInput -> State TurnstileState TurnstileOutput
+turnSX = state . turn where
+    turn Coin _        = (Thank, Unlocked)
+    turn Push Unlocked = (Open, Locked)
+    turn Push Locked   = (Tut, Locked)
+
+
+monday :: TurnstileState -> ([TurnstileOutput], TurnstileState)
+monday s0 = 
+    let (a1, s1) = coin s0
+        (a2, s2) = push s1
+        (a3, s3) = push s2
+        (a4, s4) = coin s3
+        (a5, s5) = push s4
+    in ([a1, a2, a3, a4, a5], s5)
+
+tuesday :: TurnstileState -> ([TurnstileOutput], TurnstileState)
+tuesday s0 =
+    let (a1, s1) = regularPerson s0
+        (a2, s2) = hastyPerson s1
+        (a3, s3) = distractedPerson s2
+        (a4, s4) = hastyPerson s3
+    in ( a1++a2++a3++a4, s4 )
+
+regularPerson :: TurnstileState -> ([TurnstileOutput], TurnstileState)
+regularPerson s0 =
+    let (a1, s1) = coin s0
+        (a2, s2) = push s1
+    in ([a1, a2], s2)
+
+regularPersonSX :: State TurnstileState [TurnstileOutput] 
+regularPersonSX = sequence [ coinSX, pushSX ]
+
+distractedPerson :: TurnstileState -> ([TurnstileOutput], TurnstileState)
+distractedPerson s0 =
+    let (a1, s1) = coin s0
+    in ([a1], s1)
+
+distractedPersonSX :: State TurnstileState [TurnstileOutput] 
+distractedPersonSX = sequence [ coinSX ]
+
+hastyPerson :: TurnstileState -> ([TurnstileOutput], TurnstileState)
+hastyPerson s0 = 
+    let (a1, s1) = push s0
+    in if a1==Tut then
+        let (as2, s2) = regularPerson s1
+        in (a1:as2, s2) 
+    else 
+        ([a1],s1)
+
+hastyPersonSX :: State TurnstileState [TurnstileOutput] 
+hastyPersonSX = do
+    pushSX >>= ( \thingy -> (if thingy == Tut then regularPersonSX else sequence []) 
+                                >>= ( \lastThingy -> return (thingy:lastThingy) ) )
+                                             
+
+
+luckyPair :: Bool -> TurnstileState -> (Bool, TurnstileState)
+luckyPair isRegularPerson s0 = 
+    let (a1, s1) = if isRegularPerson then regularPerson s0 else distractedPerson s0
+        (a2, s2) = push s1
+    in (a2 == Open, s2)
+
+
+luckyPairSX :: Bool -> State TurnstileState Bool
+luckyPairSX isRegularPerson = do
+    if isRegularPerson then regularPersonSX else distractedPersonSX
+    a2 <- pushSX
+    return (a2 == Open)
+
+
+testTurnstile :: State TurnstileState Bool
+testTurnstile = do
+  originalState <- get
+  put Locked
+  check1 <- pushSX
+  coinSX
+  check1' <- get
+  put Unlocked
+  check2 <- pushSX
+  coinSX
+  check2' <- get
+  put originalState
+  return (check1 == Tut && check1' == Unlocked && check2 == Open && check2' == Unlocked)
+
+--newtype State s a = State { runState :: s -> (a, s) }
+
+-- tick :: State Int Int
+-- tick = do
+--     n <- get
+--     put (n+1)
+--     return n
+
+-- type Stack = [Int]
 
 -- pop :: State Stack Int
 -- pop = State $ \(x:xs) -> (x,xs)
@@ -25,6 +213,7 @@ type Stack = [Integer]
 --     push 3
 --     a <- pop
 --     pop
+
 -- pop :: Stack -> (Integer, Stack)
 -- pop (x:xs) = (x,xs)
 
